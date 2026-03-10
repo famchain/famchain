5190f3e3b8e9fc043a523e8ce355671e468920b1
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-03-10 14:37:07 -0700

    skip comments

--- src/fam1.s
+++ src/fam1.s
@@ -38,12 +38,15 @@ capture_loop:
 	beq		x7, x8, end_capture	# LF. termination
 	li		x8, 13
 	beq		x7, x8, end_capture	# CR. termination
+
 cont_capture:
 	sb		x30, 0(x6)		# store in buffer
 	addi		x6, x6, 1		# advance capture pointer
 	mv		x7, x30			# set last char
 	j		capture_loop		# repeat
+
 end_capture:
+	jal		x1, pass1		# first pass
 	jal		x1, output		# output data
 	jal		x1, exit		# call exit function
 
@@ -51,15 +54,52 @@ end_capture:
 # Helper Functions
 # ──────────────────────────────────────────────────────────────────────────────
 
+# Input x5/x6 start/end of input buffer. The function updates them in place.
+# Min clobber x27
+pass1:
+	mv		x29, x5
+	mv		x30, x6
+pass1_loop:
+	beq		x29, x6, pass1_end_loop
+
+        lb		x28, 0(x29)      # x28 = *src
+	addi		x29, x29, 1
+
+	li		x27, 35
+	beq		x27, x28, skip_comment
+
+        sb		x28, 0(x30)      # *dst = x28
+	addi		x30, x30, 1
+	j		pass1_loop
+
+skip_comment:
+	beq		x29, x6, pass1_end_loop
+	lb		x27, 0(x29)
+	li		x28, 10
+	beq		x28, x27, pass1_loop
+	li		x28, 13
+	beq             x28, x27, pass1_loop
+	addi            x29, x29, 1
+	j		skip_comment
+
+pass1_end_loop:
+	mv		x5, x29
+	mv		x6, x30
+	ret
+
+
+
 # Input: x2 (UART base)
 # Input: x5 (start of work buffer)
 # Input: x6 (pointer to the end of work buffer)
 # Min clobber: x28
 output:
 	mv		x30, x5
+
 output_loop:
 	beq		x30, x6, end_output
 	lbu		x29, 0(x30)
+
 begin_write:
 	lbu		x28, 5(x2)
 	andi		x28, x28, 0x20		# mask
@@ -68,11 +108,12 @@ begin_write:
 	li		x28, 13
 	bne		x29, x28, not_cr
 	li		x29, 10
-not_cr:
 
+not_cr:
 	sb		x29, 0(x2)		# send to UART
 	addi		x30, x30, 1		# advance
 	j		output_loop
+
 end_output:
 	ret
 
@@ -90,14 +131,17 @@ read_uart:
 	bne		x29, x30, no_dot
 	li		x29, 1
 	ret
+
 no_dot:
 	li		x29, 0
 	ret
 
+# No inputs/Outputs/return
 exit:
     li      x30, 0x100000          # QEMU Virt Test Device
     li      x29, 0x5555            # Shutdown command
     sw      x29, 0(x30)
+
 final_spin:
     wfi
     j       final_spin
