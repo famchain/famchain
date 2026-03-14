6ebf1d7d9420240b899f6407cdc61b830db21699
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-03-13 20:58:17 -0700

    remove \r

--- scripts/fam0.sh
+++ scripts/fam0.sh
@@ -2,5 +2,6 @@
 
 riscv64-unknown-elf-as -o bin/fam0.o src/fam0.S
 riscv64-unknown-elf-objcopy -O binary bin/fam0.o bin/fam0.bin
-cat ./tmp/test.fam0 | qemu-system-riscv64 -machine virt -nographic  -bios none -device loader,file=./bin/fam0.bin,addr=0x80000000 | tee ./bin/test.bin > /dev/null
+
+(cat tmp/test.fam0; printf '\004') | qemu-system-riscv64 -machine virt -nographic  -bios none -device loader,file=./bin/fam0.bin,addr=0x80000000 | tee ./bin/test.bin > /dev/null
 hexdump -C bin/test.bin
