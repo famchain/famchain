e2600b2c13c0c40df42cc4c530c2e5e55fe2828a
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-03-14 12:37:47 -0700

    famos

---
+++ scripts/famos.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+riscv64-unknown-elf-as -o bin/famos.o src/famos.S || exit 1
+riscv64-unknown-elf-objcopy -O binary bin/famos.o bin/famos.bin || exit 1
+
+qemu-system-riscv64 \
+	-machine virt \
+	-nographic \
+	-bios none \
+	-device loader,file=./bin/famos.bin,addr=0x80000000 
+
