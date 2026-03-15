6d5685811366e47c1e0e665a1d3d81ac6d3ea449
Christopher Gilliard <christopher.gilliard@gmail.com>
2026-03-14 18:33:36 -0700

    additional disk

--- scripts/disk.sh
+++ scripts/disk.sh
@@ -3,11 +3,20 @@
 riscv64-unknown-elf-as -o bin/disk.o src/disk.S || exit 1
 riscv64-unknown-elf-objcopy -O binary bin/disk.o bin/disk.bin || exit 1
 
+#qemu-system-riscv64 \
+#    -machine virt \
+#    -nographic \
+#    -bios none \
+#    -device loader,file=./bin/disk.bin,addr=0x80000000 \
+#    -drive file=./tmp/disk.img,if=none,format=raw,id=dr0 \
+#    -device virtio-blk-device,drive=dr0
+
 qemu-system-riscv64 \
     -machine virt \
     -nographic \
     -bios none \
+    -smp 1 \
     -device loader,file=./bin/disk.bin,addr=0x80000000 \
     -drive file=./tmp/disk.img,if=none,format=raw,id=dr0 \
-    -device virtio-blk-device,drive=dr0
+    -device virtio-blk-device,drive=dr0,bus=virtio-mmio-bus.0
 
